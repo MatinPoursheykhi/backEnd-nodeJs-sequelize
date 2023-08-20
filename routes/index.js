c79893ed41ddce587d2f1bed60363eb5ba0b0109
Mateen Poursheykhi <90558887+MatinPoursheykhi@users.noreply.github.com>
2023-08-20 16:10:34 +0330

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 module.exports = (app) => {
     app.use(function (req, res, next) {
-        // let corsOptions = { origin: ['http://localhost:5173'] }
-        // res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173")
         res.setHeader("Access-Control-Allow-Origin", "*")
         res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Max-Age", "1800");
