cdf587c456cd79c336aa4833d28ab414cba732e4
Mateen Poursheykhi <90558887+MatinPoursheykhi@users.noreply.github.com>
2023-08-20 16:11:59 +0330

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,3 @@
-// we have to connect back to dataBase with sequelize like down below exampel
 const db = {}
 const { Sequelize, Op, QueryTypes } = require('sequelize');
 const connection = new Sequelize('test1', 'sa', '123', {
@@ -30,30 +29,4 @@ db.Op = Op
 
 db.Users = require('./users.model')(connection, Sequelize)
 
-
 module.exports = db
-
-
-
-
-
-
-
-
-
-
-
-
-
-// config for database and connection via sql directly
-// let sql = require("mssql");
-// let config = {
-//     user: 'sa',
-//     password: '123',
-//     server: 'localhost',
-//     database: 'test1'
-// };
-// config = dataBase configs
-// sql.connect(config, function (err) {
-//     if (err) console.log('errrrrror');
-// })
